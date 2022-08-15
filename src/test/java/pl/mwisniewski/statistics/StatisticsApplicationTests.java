8acd8531278c256c630334ac2b9647214074ac9a
questras <michal.wisniewski422@gmail.com>
2022-08-15 11:09:25 +0200

    Project Setup.

---
+++ src/test/java/pl/mwisniewski/statistics/StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwisniewski.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
